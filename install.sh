076a109f72854493a6cbef52ff9f5ee61d8ffb40
Malander <marimichele26@gmail.com>
2015-10-29 16:55:15 +0100

    first commit

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ cd ../;
 npm install elixir-juice --save-dev;
 bower init;
 cp ./falcon/.bowerrc . ;
-cp ./falcon/.gulpfile.js ./gulpfile_falcon.js ;
+cp ./falcon/.gulpfile_falcon.js ./gulpfile_falcon.js ;
