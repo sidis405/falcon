e135969b4320d8a16989ae4e73a6372924e037a5
Malander <marimichele26@gmail.com>
2015-11-04 15:44:11 +0100

    first commit

--- install.sh
+++ install.sh
@@ -3,10 +3,17 @@
 
 cd ../;
 npm install;
-npm install elixir-juice gulp-rename del laravel-elixir-jade -save-dev;
+npm install elixir-juice gulp-rename del laravel-elixir-jade --save-dev;
 bower init;
 cp ./falcon/.bowerrc . ;
 cp ./falcon/gulpfile_falcon.js ./gulpfile_falcon.js ;
+
+
+cp ./falcon/jade ./resources/assets ;
+cp ./falcon/js ./resources/assets ;
+cp ./falcon/less ./resources/assets ;
+
+
 echo /falcon >> .gitignore;
 echo /resources/bower_components >> .gitignore;
 echo /resources/assets/vendor >> .gitignore;
